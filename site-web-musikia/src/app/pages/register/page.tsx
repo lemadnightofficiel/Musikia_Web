e44247a45943507a118968ce2f2182aca7c4135e
lemadnightofficiel <matthieu.caron25@gmail.com>
2025-01-16 11:54:08 +0100

    modified register and added password conditions

--- site-web-musikia/src/app/pages/register/page.tsx
+++ site-web-musikia/src/app/pages/register/page.tsx
@@ -6,8 +6,10 @@ import Link from "next/link";
 import Navbar from "@/app/components/NavBar";
 import Footer from "@/app/components/Footer";
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-0
+import { useRouter } from 'next/navigation';
+
 const RegisterPage = () => {
+  const router = useRouter();
   const [email, setEmail] = useState('');
   const [instrument, setInstrument] = useState('');
   const [password, setPassword] = useState('');
@@ -15,6 +17,7 @@ const RegisterPage = () => {
   const [passwordMatch, setPasswordMatch] = useState(true);
   const [showPassword, setShowPassword] = useState<boolean>(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState<boolean>(false);
+  const [showPasswordRequirements, setShowPasswordRequirements] = useState<boolean>(false);
 
   const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
@@ -33,6 +36,7 @@ const RegisterPage = () => {
       return;
     }
     console.log("Form submitted:", { email, instrument, password });
+    router.push('/pages/home');
   };
 
   return (
@@ -42,37 +46,34 @@ const RegisterPage = () => {
         <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-md relative">
           <h1 className="text-4xl font-bold mb-6 text-center text-[var(--h1-color)]">Inscription</h1>
           <form className="flex flex-col" onSubmit={handleSubmit}>
-            <input type="email" placeholder="Email" className="border border-gray-300 rounded p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500"  value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            <input type="email" placeholder="Email" className="border border-gray-300 rounded p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500" value={email} onChange={(e) => setEmail(e.target.value)} required/>
             <div className="relative mb-4">
-              <input type={showPassword ? "text" : "password"} placeholder="Mot de passe" className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500" required value={password} onChange={handlePasswordChange}/>
+              <input type={showPassword ? "text" : "password"} placeholder="Mot de passe" className="border border-gray-300 rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500" required value={password} onChange={handlePasswordChange} onFocus={() => setShowPasswordRequirements(true)}onBlur={() => setShowPasswordRequirements(false)}/>
               <button type="button" className="absolute right-3 top-3" onClick={() => setShowPassword(!showPassword)}>
-                {showPassword ? (
-                  <FaEye />
-                ) : (
-                  <FaEyeSlash />
-                )}
+                {showPassword ? <FaEye /> : <FaEyeSlash />}
               </button>
             </div>
+            {showPasswordRequirements && (
+              <p className="text-red-500 text-sm mb-2">
+                Le mot de passe doit contenir au moins 8 caractères, une majuscule, un chiffre et un caractère spécial.
+              </p>
+            )}
             <div className="relative mb-4">
               <input type={showConfirmPassword ? "text" : "password"} placeholder="Confirmer le mot de passe" className={`border rounded p-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 ${passwordMatch ? 'border-gray-300' : 'border-red-500'}`} required value={confirmPassword} onChange={handleConfirmPasswordChange}/>
-              { !passwordMatch && (
-                <p className="text-red-500 text-sm">Les mots de passe ne correspondent pas.</p>
+              {!passwordMatch && (
+                <p className="text-red-500 text-sm mt-3">Les mots de passe ne correspondent pas.</p>
               )}
               <button type="button" className="absolute right-3 top-3" onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
-                {showConfirmPassword ? (
-                  <FaEye />
-                ) : (
-                  <FaEyeSlash />
-                )}
+                {showConfirmPassword ? <FaEye /> : <FaEyeSlash />}
               </button>
             </div>
-              <select value={instrument} onChange={(e) => setInstrument(e.target.value)} className="border border-gray-300 rounded p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500" required>
-                <option value="" disabled>Choisissez votre instrument</option>
-                <option value="Guitare">Guitare</option>
-                <option value="Piano">Piano</option>
-                <option value="Batterie">Batterie</option>
-                <option value="Violon">Violon</option>
-              </select>
+            <select value={instrument} onChange={(e) => setInstrument(e.target.value)} className="border border-gray-300 rounded p-2 mb-4 focus:outline-none focus:ring-2 focus:ring-indigo-500" required>
+              <option value="" disabled>Choisissez votre instrument</option>
+              <option value="Guitare">Guitare</option>
+              <option value="Piano">Piano</option>
+              <option value="Batterie">Batterie</option>
+              <option value="Violon">Violon</option>
+            </select>
             <button type="submit" className="bg-[var(--btn-bg)] text-[var(--btn-text)] hover:bg-[var(--btn-hover)] py-2 px-4 rounded transition duration-300">
               S&apos;inscrire
             </button>
