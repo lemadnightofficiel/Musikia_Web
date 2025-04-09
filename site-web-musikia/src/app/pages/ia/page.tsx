c876c77ba8b8d76b0cd9b047181b2f7fab4feb5c
Yohore Emmanuel <emmanuel.yohore@ynov.com>
2025-04-09 15:39:55 +0200

    forme

--- site-web-musikia/src/app/pages/ia/page.tsx
+++ site-web-musikia/src/app/pages/ia/page.tsx
@@ -73,12 +73,12 @@ const IAPage: React.FC = () => {
       <Navbar />
       <main className="flex-grow container mx-auto px-6 py-8 pt-24">
         <h1 className="text-4xl mt-4 mb-6 text-center text-black">Notre transcripteur par IA</h1>
-        <p className="text-lg text-center mb-8 text-black-600">
+        <p className="text-lg text-center mb-8 text-black">
           Transcrivez votre Musique en seulement quelques minutes avec l&apos;IA Musikia. Il suffit de télécharger votre musique au format MP3 et l&apos;IA se charger du reste!
         </p>
         {!selectedFile ? (
           <div>
-            <h3 className="text-2xl font-bold mb-6 text-center">Convertissez votre musique en partition</h3>
+            <h3 className="text-2xl font-bold mb-6 text-center text-black">Convertissez votre musique en partition</h3>
             <div className="max-w-4xl mx-auto bg-white p-6 rounded-lg shadow-lg">
               <FileUpload />
             </div>
@@ -87,15 +87,15 @@ const IAPage: React.FC = () => {
                 <img src="/images/intro/image_intro_3.png" alt="Illustration de conversion musicale" className="w-full rounded-xl" />
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-4">Comment ça marche ?</h3>
-                <p className="mb-2">Notre IA se découpe en 4 parties :</p>
-                <ol className="list-decimal pl-6 space-y-2">
+                <h3 className="text-3xl font-semibold text-gray-800">Comment ça marche ?</h3>
+                <p className="text-lg text-gray-600">Notre IA se découpe en 4 parties :</p>
+                <ol className="list-decimal list-inside text-gray-600 space-y-2">
                   <li>IA récupère la musique que vous souhaitez transcrire.</li>
                   <li>La musique est découpée en différentes pistes audio. Une piste audio est dédiée par instrument.</li>
                   <li>Grâce à son immense base de données, l&apos;IA repère chaque changement de son et lui attribue une note/accord.</li>
                   <li>L&apos;IA écrit ses résultats sous forme de partition. Une partition est générée pour chaque instrument présent dans la musique.</li>
                 </ol>
-                <p className="mt-4">Si vous souhaitez en savoir plus, vous pouvez consulter nos articles sur le fonctionnement de Musikia.</p>
+                <p className="text-lg text-gray-600">Si vous souhaitez en savoir plus, vous pouvez consulter nos articles sur le fonctionnement de Musikia.</p>
                 <div className="mt-4">
                   <button className="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 transition-all duration-300">
                     Voir nos réseaux
@@ -109,7 +109,7 @@ const IAPage: React.FC = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-16">
               <div className="bg-white p-6 rounded-lg shadow-lg h-fit">
                 <div className="mb-4 flex justify-between items-center">
-                  <h2 className="text-xl font-bold">Fichier sélectionné</h2>
+                  <h2 className="text-xl font-bold text-black">Fichier sélectionné</h2>
                   <button 
                     onClick={() => removeFile()} 
                     className="text-red-500 hover:text-red-700 font-medium flex items-center gap-1"
@@ -118,8 +118,8 @@ const IAPage: React.FC = () => {
                   </button>
                 </div>
                 <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-                  <p><strong>Nom:</strong> {selectedFile.name}</p>
-                  <p><strong>Durée:</strong> {selectedFile.duration}</p>
+                  <p className="text-black"><strong>Nom:</strong> {selectedFile.name}</p>
+                  <p className="text-black"><strong>Durée:</strong> {selectedFile.duration}</p>
                   <audio controls className="w-full mt-2">
                     <source src={selectedFile.url} type="audio/mp3" />
                   </audio>
@@ -129,7 +129,7 @@ const IAPage: React.FC = () => {
                 </button>
               </div>
               <div className="bg-white p-6 rounded-lg shadow-lg h-fit">
-                <h2 className="text-xl font-bold mb-4">Choisissez un instrument</h2>
+                <h2 className="text-xl font-bold mb-4 text-black">Choisissez un instrument</h2>
                 {instruments.map((instrument) => (
                   <div key={instrument} className="mb-4 border-b pb-2">
                     <button
@@ -141,8 +141,8 @@ const IAPage: React.FC = () => {
                     </button>
                     {openInstrument === instrument && (
                       <div className="mt-2 p-4 bg-gray-100 rounded-lg shadow-md transition-all duration-300 ease-in-out">
-                        <p><strong>Partition pour:</strong> {instrument}</p>
-                        <p><strong>Fichier:</strong> {selectedFile.name}</p>
+                        <p className="text-black"><strong>Partition pour:</strong> {instrument}</p>
+                        <p className="text-black"><strong>Fichier:</strong> {selectedFile.name}</p>
                         <audio controls className="w-full mt-2">
                           <source src={selectedFile.url} type="audio/mp3" />
                         </audio>
@@ -161,15 +161,15 @@ const IAPage: React.FC = () => {
                 <img src="/images/intro/image_intro_3.png" alt="Illustration de conversion musicale" className="w-full rounded-xl" />
               </div>
               <div>
-                <h3 className="text-2xl font-bold mb-4">Comment ça marche ?</h3>
-                <p className="mb-2">Notre IA se découpe en 4 parties :</p>
-                <ol className="list-decimal pl-6 space-y-2">
+                <h3 className="text-3xl font-semibold text-gray-800">Comment ça marche ?</h3>
+                <p className="text-lg text-gray-600">Notre IA se découpe en 4 parties :</p>
+                <ol className="list-decimal list-inside text-gray-600 space-y-2">
                   <li>IA récupère la musique que vous souhaitez transcrire.</li>
                   <li>La musique est découpée en différentes pistes audio. Une piste audio est dédiée par instrument.</li>
                   <li>Grâce à son immense base de données, l&apos;IA repère chaque changement de son et lui attribue une note/accord.</li>
                   <li>L&apos;IA écrit ses résultats sous forme de partition. Une partition est générée pour chaque instrument présent dans la musique.</li>
                 </ol>
-                <p className="mt-4">Si vous souhaitez en savoir plus, vous pouvez consulter nos articles sur le fonctionnement de Musikia.</p>
+                <p className="text-lg text-gray-600">Si vous souhaitez en savoir plus, vous pouvez consulter nos articles sur le fonctionnement de Musikia.</p>
                 <div className="mt-4">
                   <button className="px-6 py-3 bg-red-600 text-white font-semibold rounded-lg shadow-md hover:bg-red-700 transition-all duration-300">
                     Voir nos réseaux
